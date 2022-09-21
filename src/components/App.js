f69470e0cfadb033343b829e35a70d73c96cc710
EstherWangui <esther.nganga@student.moringaschool.com>
2022-09-21 12:28:28 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,13 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <div id="home">
+      <h1>Home</h1>
+      </div>
       {/* add your <About> component here */}
+      <div id="about">
+      <h1>About</h1>
+      </div>
     </div>
   );
 }
